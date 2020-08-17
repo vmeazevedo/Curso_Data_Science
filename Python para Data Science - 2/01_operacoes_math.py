6ef4eb5d3fb8b7ad634442714618ded2aedb2edd
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 18:09:11 -0300

    Create 01_operacoes_math.py

---
+++ Python para Data Science - 2/01_operacoes_math.py
@@ -0,0 +1,29 @@
+#adição
+print(2+2)
+print()
+
+#subtração
+print(3-2)
+print()
+
+#multiplicação
+print(2*2)
+print()
+
+#divisão
+print(10/3)
+print(10//3)
+print()
+
+#exponenciação
+print(2**3)
+print()
+
+#resto da divisão
+print(10%3)
+print(10%2)
+print()
+
+#expressões matematicas
+print(5*2 + 3*2)
+print(5* (2 + 3) *2)
