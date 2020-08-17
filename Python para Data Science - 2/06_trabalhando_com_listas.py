6b1586ca59f4457bee03e3f5033fd1ea13d3cf2b
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 20:08:06 -0300

    Update 06_trabalhando_com_listas.py

---
+++ Python para Data Science - 2/06_trabalhando_com_listas.py
@@ -0,0 +1,15 @@
+
+# Criando listas
+acessorios = [ 'Rodas de liga', 'Trava elétrica', 'Piloto Automático', 'Banco de couro']
+print(acessorios)
+
+# Lista com tipos de dados variados
+carro_1 = ['Jetta Variant', 'Motor 4.0 Turbo', 2003, 44410.0, False]
+carro_2 = ['Passat', 'Motor Diesel', 1991, 5712.0, False]
+
+print(carro_1)
+print(carro_2)
+
+# Listas dentro de listas
+carros = [carro_1, carro_2]
+print(carros)
