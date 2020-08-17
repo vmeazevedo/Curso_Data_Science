dfa7fbf85b22c7c7d3341d3e1abbd9fcddfaf0cb
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 17:19:18 -0300

    Create 00_funcao.py

---
+++ Python para Data Science - 1/Códigos/00_funcao.py
@@ -0,0 +1,12 @@
+nome = 'Guilherme'
+idade = 28
+
+print(f"O nome é {nome} e sua idade é {idade} anos")
+
+#Criando minha primeira função
+def saudacao():
+    nome = input("Qual o seu nome? ")
+    print(f"Olá {nome}")
+
+saudacao()
+
