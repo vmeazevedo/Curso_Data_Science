d108a797b94d396b65a928d82c1a047020ed4d15
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 19:08:26 -0300

    Create 03_tipos_de_dados.py

---
+++ Python para Data Science - 2/03_tipos_de_dados.py
@@ -0,0 +1,29 @@
+# Numeros
+ano_atual = 2019
+km_total = 44410.0
+print(type(ano_atual))
+print(type(km_total))
+
+# Booleanos
+zero_km = True
+print(type(zero_km))
+zero_km = False
+print(type(zero_km))
+
+# String
+nome = 'Jetta Variant'
+print(type(nome))
+
+nome = "Jetta Variant"
+print(type(nome))
+
+carro = '''
+    Nome
+    Idade
+    Nota
+'''
+print(type(carro))
+
+# None
+quilometragem = None
+print(type(quilometragem))
