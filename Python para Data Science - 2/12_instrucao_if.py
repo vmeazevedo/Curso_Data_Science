a6bb15f6668e565af3e9e5be65f6cce13b75471f
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 22:15:13 -0300

    Create 12_instrucao_if.py

---
+++ Python para Data Science - 2/12_instrucao_if.py
@@ -0,0 +1,32 @@
+# 1º item da lista - Nome do veículo
+# 2º item da lista - Ano de fabricação
+# 3º item da lista - Veículo é zero km?
+
+dados = [
+    ['Jetta Variant', 2003, False],
+    ['Passat', 1991, False],
+    ['Crossfox', 1990, False],
+    ['DS5', 2009, True],
+    ['Aston Martin DB4', 2006, False],
+    ['Palio Weekend', 2012, False],
+    ['A5', 2019, True],
+    ['Série 3 Cabrio', 2009, False],
+    ['Dodge Jorney', 2019, False],
+    ['Carens', 2011, False]
+]
+
+zero_km_Y = []
+
+# Caso seja True
+for lista in dados:
+    if (lista[2] == True):
+        zero_km_Y.append(lista)
+print(zero_km_Y)
+
+#Caso seja False
+zero_km_N = []
+
+for lista in dados:
+    if(lista[2] == False):
+        zero_km_N.append(lista)
+print(zero_km_N)
