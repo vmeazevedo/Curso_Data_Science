311f20e423205a3f49d477decbdecc3475526a44
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-17 21:00:35 -0300

    Create 08_selecoes_em_listas.py

---
+++ Python para Data Science - 2/08_selecoes_em_listas.py
@@ -0,0 +1,17 @@
+# A[i]
+acessorios = ['Rodas de liga', 'Trava elétrica', 'Piloto Automático', 'Banco de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva']
+print(acessorios[0])
+print(acessorios[1])
+print(acessorios[-1])
+
+carro_1 = [['Jetta Variant', 'Motor 4.0 Turbo', 2003, 44410.0, False, ['Rodas de liga', 'Trava elétrica', 'Piloto Automático'], 88078.64],['Passat', 'Motor Diesel', 1991, 5712.0, False, ['Central multimidia', 'Teto panoramico', 'Freio ABS'], 106161.94]]
+print(carro_1[0])
+print(carro_1[0][0])
+print(carro_1[0][-2][1])
+
+
+# A[i:J]
+acessorios = ['Rodas de liga', 'Trava elétrica', 'Piloto Automático', 'Banco de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva']
+print(acessorios[2:5])
+print(acessorios[2:])
+print(acessorios[:5])
