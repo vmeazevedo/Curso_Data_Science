f9c0d4666479d6c069643b656e9a4cb7fbfca7d6
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-18 11:02:43 -0300

    Update 02_selecoes_tuplas.py

--- Python para Data Science - 3/02_selecoes_tuplas.py
+++ Python para Data Science - 3/02_selecoes_tuplas.py
@@ -6,6 +6,9 @@ print(nomes_carros[0])
 print(nomes_carros[-1])
 print(nomes_carros[1:3])
 
+# Identificando o indice de um nome em uma tupla
+print(nomes_carros.index('Passat'))
+
 
 # Acessando indices em uma tupla com uma tupla dentro
 nomes_carros = ('Jetta Variant', 'Passat', 'Crossfox', 'DS5',('Fusca', 'Gol', 'C4'))
