5fea698ae5430568bf198e6a5686ae9e7b492e11
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-22 19:33:35 -0300

    Update 06_criando_agrupamentos.py

--- 03_Python Pandas - Tratando e analisando dados/06_criando_agrupamentos.py
+++ 03_Python Pandas - Tratando e analisando dados/06_criando_agrupamentos.py
@@ -10,36 +10,36 @@ print(dados.head(10))
 # dados = dados['Bairro'].unique()
 
 # Criando uma lista com todos os bairros disponíveis
-bairros = ['Copacabana', 'Jardim Botânico', 'Centro', 'Higienópolis', 'Cachambi'
- 'Barra da Tijuca', 'Ramos', 'Grajaú', 'Lins de Vasconcelos', 'Taquara'
- 'Freguesia (Jacarepaguá)', 'Tijuca', 'Olaria', 'Ipanema', 'Campo Grande'
- 'Botafogo', 'Recreio dos Bandeirantes', 'Leblon', 'Jardim Oceânico'
- 'Humaitá', 'Península', 'Méier', 'Vargem Pequena', 'Maracanã', 'Jacarepaguá'
- 'São Conrado', 'Vila Valqueire', 'Gávea', 'Cosme Velho', 'Bonsucesso'
- 'Todos os Santos', 'Laranjeiras', 'Itanhangá', 'Flamengo', 'Piedade', 'Lagoa'
- 'Catete', 'Jardim Carioca', 'Benfica', 'Glória', 'Praça Seca', 'Vila Isabel'
- 'Engenho Novo', 'Engenho de Dentro', 'Pilares', 'Água Santa', 'São Cristóvão'
- 'Ilha do Governador', 'Jardim Sulacap', 'Oswaldo Cruz', 'Vila da Penha'
- 'Anil', 'Vargem Grande', 'Tanque', 'Vaz Lobo', 'Madureira'
- 'São Francisco Xavier', 'Pechincha', 'Leme', 'Irajá', 'Quintino Bocaiúva'
- 'Urca', 'Penha', 'Gardênia Azul', 'Rio Comprido', 'Andaraí', 'Santa Teresa'
- 'Inhaúma', 'Marechal Hermes', 'Curicica', 'Santíssimo', 'Moneró', 'Camorim'
- 'Cascadura', 'Praia da Bandeira', 'Saúde', 'Joá', 'Realengo', 'Fátima'
- 'Inhoaíba', 'Rocha', 'Jardim Guanabara', 'Jabour', 'Braz de Pina'
- 'Praça da Bandeira', 'Vila Kosmos', 'Vista Alegre', 'Encantado', 'Campinho'
- 'Guaratiba', 'Riachuelo', 'Bangu', 'Lapa', 'Catumbi', 'Penha Circular'
- 'Abolição', 'Tomás Coelho', 'Colégio', 'Pavuna', 'Santa Cruz'
- 'Alto da Boa Vista', 'Cidade Nova', 'Bento Ribeiro', 'Estácio'
- 'Jardim América', 'Cordovil', 'Caju', 'Pedra de Guaratiba', 'Padre Miguel'
- 'Paciência', 'Del Castilho', 'Arpoador', 'Sampaio', 'Anchieta', 'Icaraí'
- 'Senador Vasconcelos', 'Rocha Miranda', 'Gamboa', 'Maria da Graça'
- 'Barra de Guaratiba', 'Vicente de Carvalho', 'Paquetá', 'Largo do Machado'
- 'Parada de Lucas', 'Freguesia (Ilha do Governador)', 'Portuguesa'
- 'Guadalupe', 'Parque Anchieta', 'Turiaçu', 'Pitangueiras', 'Vila Militar'
- 'Vidigal', 'Senador Camará', 'Usina', 'Vigário Geral', 'Cosmos', 'Jacaré'
- 'Cocotá', 'Honório Gurgel', 'Engenho da Rainha', 'Cachamorra', 'Zumbi', 'Tauá'
- 'Santo Cristo', 'Ribeira', 'Magalhães Bastos', 'Cacuia', 'Bancários'
- 'Cavalcanti', 'Rio da Prata', 'Cidade Jardim', 'Coelho Neto']
+bairros = ['Copacabana', 'Jardim Botânico', 'Centro', 'Higienópolis', 'Cachambi']
+#  'Barra da Tijuca', 'Ramos', 'Grajaú', 'Lins de Vasconcelos', 'Taquara'
+#  'Freguesia (Jacarepaguá)', 'Tijuca', 'Olaria', 'Ipanema', 'Campo Grande'
+#  'Botafogo', 'Recreio dos Bandeirantes', 'Leblon', 'Jardim Oceânico'
+#  'Humaitá', 'Península', 'Méier', 'Vargem Pequena', 'Maracanã', 'Jacarepaguá'
+#  'São Conrado', 'Vila Valqueire', 'Gávea', 'Cosme Velho', 'Bonsucesso'
+#  'Todos os Santos', 'Laranjeiras', 'Itanhangá', 'Flamengo', 'Piedade', 'Lagoa'
+#  'Catete', 'Jardim Carioca', 'Benfica', 'Glória', 'Praça Seca', 'Vila Isabel'
+#  'Engenho Novo', 'Engenho de Dentro', 'Pilares', 'Água Santa', 'São Cristóvão'
+#  'Ilha do Governador', 'Jardim Sulacap', 'Oswaldo Cruz', 'Vila da Penha'
+#  'Anil', 'Vargem Grande', 'Tanque', 'Vaz Lobo', 'Madureira'
+#  'São Francisco Xavier', 'Pechincha', 'Leme', 'Irajá', 'Quintino Bocaiúva'
+#  'Urca', 'Penha', 'Gardênia Azul', 'Rio Comprido', 'Andaraí', 'Santa Teresa'
+#  'Inhaúma', 'Marechal Hermes', 'Curicica', 'Santíssimo', 'Moneró', 'Camorim'
+#  'Cascadura', 'Praia da Bandeira', 'Saúde', 'Joá', 'Realengo', 'Fátima'
+#  'Inhoaíba', 'Rocha', 'Jardim Guanabara', 'Jabour', 'Braz de Pina'
+#  'Praça da Bandeira', 'Vila Kosmos', 'Vista Alegre', 'Encantado', 'Campinho'
+#  'Guaratiba', 'Riachuelo', 'Bangu', 'Lapa', 'Catumbi', 'Penha Circular'
+#  'Abolição', 'Tomás Coelho', 'Colégio', 'Pavuna', 'Santa Cruz'
+#  'Alto da Boa Vista', 'Cidade Nova', 'Bento Ribeiro', 'Estácio'
+#  'Jardim América', 'Cordovil', 'Caju', 'Pedra de Guaratiba', 'Padre Miguel'
+#  'Paciência', 'Del Castilho', 'Arpoador', 'Sampaio', 'Anchieta', 'Icaraí'
+#  'Senador Vasconcelos', 'Rocha Miranda', 'Gamboa', 'Maria da Graça'
+#  'Barra de Guaratiba', 'Vicente de Carvalho', 'Paquetá', 'Largo do Machado'
+#  'Parada de Lucas', 'Freguesia (Ilha do Governador)', 'Portuguesa'
+#  'Guadalupe', 'Parque Anchieta', 'Turiaçu', 'Pitangueiras', 'Vila Militar'
+#  'Vidigal', 'Senador Camará', 'Usina', 'Vigário Geral', 'Cosmos', 'Jacaré'
+#  'Cocotá', 'Honório Gurgel', 'Engenho da Rainha', 'Cachamorra', 'Zumbi', 'Tauá'
+#  'Santo Cristo', 'Ribeira', 'Magalhães Bastos', 'Cacuia', 'Bancários'
+#  'Cavalcanti', 'Rio da Prata', 'Cidade Jardim', 'Coelho Neto']
 
 
 # Realizando uma seleção e criando um novo DataFrame
@@ -65,16 +65,13 @@ print(b)
 
 
 import matplotlib.pyplot as plt
-plt.rc('figure', figsize = (10,5))     # Configura o tamanho que a figura irá ser apresentada
+plt.rc('figure', figsize = (7,7))     # Configura o tamanho que a figura irá ser apresentada
 
 # Apresentando os valores de aluguel por bairro com o matplotlib
 fig = grupo_bairro['Valor'].mean().plot.bar(color = 'blue')
-fig.set_ylabel('Valor do Aluguel')
-fig.set_title("Valor Médio do Aluguel x Condominio por Bairro", {'fontsize':22})
-
 fig = grupo_bairro['Condominio'].mean().plot.bar(color = 'red')
-fig.set_ylabel('Valor do Condominio')
-
-
-
+fig.set_ylabel('Valor do Aluguel x Condominio')
+fig.set_xlabel('Bairro')
+fig.set_title("Valor Médio do Aluguel x Condominio por Bairro", {'fontsize':20})
 plt.show()
+
