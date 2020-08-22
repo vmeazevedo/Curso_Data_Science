5f3519fd79ab809a0c0f8992d871ff17984309a6
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-22 19:46:29 -0300

    Create 06_criando_faixas_de_valor.py

---
+++ 03_Python Pandas - Tratando e analisando dados/00_Códigos_extras/06_criando_faixas_de_valor.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+
+dados = pd.read_csv('dados\\aluguel_residencial_3.csv', sep=';')
+print(dados.head(10))
+
+# 1 e 2
+# 3 e 4
+# 5 e 6
+# 7 ou mais
+
+classes = [0,2,4,6,100]
+
+quartos = pd.cut(dados.Quartos, classes)
+print(quartos)
+
+print(pd.value_counts(quartos))
+
+labels = ['1 e 2 quartos', '3 e 4 quartos', '5 e 6 quartos', '7 quartos ou mais']
+quartos = pd.cut(dados.Quartos, classes, labels= labels)
+print(pd.value_counts(quartos))
