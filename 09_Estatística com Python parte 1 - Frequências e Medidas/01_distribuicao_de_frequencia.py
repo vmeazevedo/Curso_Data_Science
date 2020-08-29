3ea9689cf86359d6d037548e9443cb4f02c5a3a6
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-08-29 13:27:59 -0300

    Update 01_distribuicao_de_frequencia.py

--- 09_Estatística com Python parte 1 - Frequências e Medidas/01_distribuicao_de_frequencia.py
+++ 09_Estatística com Python parte 1 - Frequências e Medidas/01_distribuicao_de_frequencia.py
@@ -12,5 +12,16 @@ print(dados['Sexo'].value_counts())
 # Utilizando o normalize para apresentar o resultado em %
 print(dados['Sexo'].value_counts(normalize = True)*100)
 
+# Salvando as informações em variaveis
 frequencia = dados['Sexo'].value_counts()
 percentual = dados['Sexo'].value_counts(normalize = True)*100
+
+# Criando um novo DataFrame com as variaiveis
+dist_freq_qualitativas = pd.DataFrame({'Frequência': frequencia, 'Porcentagem (%)':percentual.round(3)})
+print(dist_freq_qualitativas.head())
+
+# Alterando os valores de 0 e 1 na nossa coluna 
+dist_freq_qualitativas.rename(index= {0: 'Masculino', 1:'Feminino'}, inplace=True)
+# Renomeando a coluna sexo novamente
+dist_freq_qualitativas.rename_axis('Sexo', axis='columns', inplace=True)
+print(dist_freq_qualitativas.head())
