651eeff04b2584ee050d33b3a992cb774491e6c1
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-09-12 14:28:52 -0300

    Create 09_filtrando_registros.sql

---
+++ 13_Introdução ao SQL com MySQL - Manipule e consulte dados/09_filtrando_registros.sql
@@ -0,0 +1,4 @@
+# Filtrando um select utilizando o comando WHERE
+SELECT * FROM tbproduto where produto = '544931';
+select * from tbcliente where cidade = 'Rio de Janeiro';
+select * from tbproduto where sabor = 'Limão';
