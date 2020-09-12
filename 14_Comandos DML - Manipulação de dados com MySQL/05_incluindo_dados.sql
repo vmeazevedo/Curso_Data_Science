6bc35d36f4b9e21c7174fb8f0069601051839d23
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-09-12 20:20:04 -0300

    Create 05_incluindo_dados.sql

---
+++ 14_Comandos DML - Manipulação de dados com MySQL/05_incluindo_dados.sql
@@ -0,0 +1,18 @@
+use vendas_sucos;
+
+insert into produtos (codigo, descritor, sabor, tamanho, embalagem, preco_lista)
+values ('1040107','Light - 350 ml - Melância', 'Melância', '350 ml', 'Lata', 4.56);
+
+select * from produtos;
+
+insert into produtos (codigo, descritor, sabor, tamanho, embalagem, preco_lista)
+values ('1040108','Light - 350 ml - Graviola', 'Graviola', '350 ml', 'Lata', 4.00);
+
+insert into produtos 
+values ('1040109','Light - 350 ml - Açai', 'Açai', '350 ml', 'Lata', 5.60);
+
+insert into produtos 
+values ('1040110','Light - 350 ml - Jaca', 'Jaca', '350 ml', 'Lata', 6.00),
+	   ('1040111','Light - 350 ml - Manga', 'Manga', '350 ml', 'Lata', 3.50);
+
+
